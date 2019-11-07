31fdc85840f26fb6fa674ea9ece91c8a16eb07ce
Brian Nicholls <nichol88@gmail.com>
2019-11-07 07:26:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,15 @@ require "yaml"
 def load_library(path)
   # code goes here
   YAML.load_file(path)
+  hash = {
+    "get_meaning": => {},
+    "get_emoticon": => {}
+  }
 end
 
 def get_japanese_emoticon
   # code goes here
+
 end
 
 def get_english_meaning
