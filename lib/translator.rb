32ffac25aed776665051e8e3a9b9a2118555a6de
Brian Nicholls <nichol88@gmail.com>
2019-11-07 09:10:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,37 +5,24 @@ def load_library(path)
   # code goes here
   hash = YAML.load_file(path)
   #pp(hash)
-
   result = {
     :get_meaning => {},
     :get_emoticon => {}
   }
-
   hash.each{ |description, emojis|
-
     if !result[:get_meaning][emojis[1]]
       result[:get_meaning][emojis[1]] = description
     end
-
     if !result[:get_emoticon][emojis[0]]
       result[:get_emoticon][emojis[0]] = emojis[1]
     end
   }
-=begin
-  hash.each{ |description, emojis|
-    if !result[:get_meaning][emojis[1]]
-      result[:get_meaning][emojis[1]] = {}
-    end
-  }
-=end
-
-  puts result
   result
 end
 
 def get_japanese_emoticon
   # code goes here
-
+  
 end
 
 def get_english_meaning
